e6d3f20ae1e6b36194eb9d5538e5528bc2f437fc
Jiraiya4 <jiraiya4x4@gmail.com>
2021-08-25 11:38:35 +0300

    clean file system and made first build

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList'
-import { robots } from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor(){
@@ -25,24 +24,20 @@ class App extends Component {
     }
 
     render(){
-        let filteredRobots = this.state.robots.filter( robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state;
+        let filteredRobots = robots.filter( robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0){
-            return <h1>LOADING</h1>
-        }
-        else{
-            return (
-                <div className='tc'>
+        return !robots.length ? <h1>LOADING</h1> :
+                (<div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
                         <CardList robots={filteredRobots}/>
                     </Scroll>    
-                </div>
-            )
+                </div>)
         }
     }
-}
+
 
 export default App;
